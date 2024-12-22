491a583549eb0b12708079f1fcc64a164493219b
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2024-12-22 19:40:57 +0100

    page

--- src/app/(pages)/studieforberedende/[...linje]/page.tsx
+++ src/app/(pages)/studieforberedende/[...linje]/page.tsx
@@ -1,8 +1,39 @@
+"use client";
+
+import { checkIfLinjeExists, getLinjeData } from "@/functions/studieforberedende/databaseFunctions";
+import { redirect, usePathname } from 'next/navigation'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card";
+
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ linje: string }>
 }) {
   const linje = (await params).linje
-  return <div>Linje: {linje.length}</div>
+  const linjeExists = await checkIfLinjeExists(linje);
+
+  if (!linjeExists) { redirect("/"); } 
+
+  const linjeData = await getLinjeData(linje);
+
+  const handleCardClick = (id: number) => {
+    redirect("/"+(id.toString()))
+  }
+
+  return (
+    <div className="">
+      <h1>{linjeData.name}</h1>
+
+      {Object.values(linjeData.year_data).map((item, key) => (
+          <Card key={item.id} className="bg-[] rounded-2xl cursor-pointer" onClick={() => handleCardClick(item.id)}>
+            <CardHeader>
+              <CardTitle>{item.name}</CardTitle>
+              <CardDescription>{item.fag}</CardDescription>
+            </CardHeader>
+          </Card>
+        ))}
+
+    </div>
+  )
 }
