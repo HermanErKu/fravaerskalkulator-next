dafc9bc9799189ce7d58416f560f34d596e3b112
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2024-12-26 21:55:05 +0100

    added divider for years

--- src/components/SubLinjeCards.tsx
+++ src/components/SubLinjeCards.tsx
@@ -34,49 +34,71 @@ const SubLinjeCards: React.FC<SubLinjeCardsProps> = ({ data }) => {
 
   return (
     <div>
-      <div className="grid grid-cols-2 gap-4">
-        {Object.values(data.filter((filterData) => filterData.year == 1)).map((linjeData) => (
-          <Card key={linjeData.id} id={"year_"+linjeData.id.toString()} className="bg-[] rounded-2xl cursor-pointer" onClick={() => handleCardClick(linjeData.id)}>
-            <CardHeader>  
-              <CardTitle>{linjeData.name}</CardTitle>
-              <CardDescription>{linjeData.year}</CardDescription>
-            </CardHeader>
-          </Card>
-        ))}
+      
+      {data.filter((filterData) => filterData.year == 1).length >= 1 && (
+      <div>
+        <h1>VG1</h1>
+        <div className="grid grid-cols-2 gap-4">
+          {Object.values(data.filter((filterData) => filterData.year == 1)).map((linjeData) => (
+            <Card key={linjeData.id} id={"year_"+linjeData.id.toString()} className="bg-[] rounded-2xl cursor-pointer" onClick={() => handleCardClick(linjeData.id)}>
+              <CardHeader>  
+                <CardTitle>{linjeData.name}</CardTitle>
+                <CardDescription>{linjeData.year}</CardDescription>
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
       </div>
+      )}
 
-      <div className="grid grid-cols-2 gap-4">
-        {Object.values(data.filter((filterData) => filterData.year == 2)).map((linjeData) => (
-          <Card key={linjeData.id} id={"year_"+linjeData.id.toString()} className="bg-[] rounded-2xl cursor-pointer" onClick={() => handleCardClick(linjeData.id)}>
-            <CardHeader>  
-              <CardTitle>{linjeData.name}</CardTitle>
-              <CardDescription>{linjeData.year}</CardDescription>
-            </CardHeader>
-          </Card>
-        ))}
+      {data.filter((filterData) => filterData.year == 2).length >= 1 && (
+     <div>
+        <h1>VG2</h1>
+        <div className="grid grid-cols-2 gap-4">
+          {Object.values(data.filter((filterData) => filterData.year == 2)).map((linjeData) => (
+            <Card key={linjeData.id} id={"year_"+linjeData.id.toString()} className="bg-[] rounded-2xl cursor-pointer" onClick={() => handleCardClick(linjeData.id)}>
+              <CardHeader>  
+                <CardTitle>{linjeData.name}</CardTitle>
+                <CardDescription>{linjeData.year}</CardDescription>
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
       </div>
+      )}
 
-      <div className="grid grid-cols-2 gap-4">
-        {Object.values(data.filter((filterData) => filterData.year == 3)).map((linjeData) => (
-          <Card key={linjeData.id} id={"year_"+linjeData.id.toString()} className="bg-[] rounded-2xl cursor-pointer" onClick={() => handleCardClick(linjeData.id)}>
-            <CardHeader>  
-              <CardTitle>{linjeData.name}</CardTitle>
-              <CardDescription>{linjeData.year}</CardDescription>
-            </CardHeader>
-          </Card>
-        ))}
+      {data.filter((filterData) => filterData.year == 3).length >= 1 && (
+      <div>
+        <h1>VG3</h1>
+        <div className="grid grid-cols-2 gap-4">
+          {Object.values(data.filter((filterData) => filterData.year == 3)).map((linjeData) => (
+            <Card key={linjeData.id} id={"year_"+linjeData.id.toString()} className="bg-[] rounded-2xl cursor-pointer" onClick={() => handleCardClick(linjeData.id)}>
+              <CardHeader>  
+                <CardTitle>{linjeData.name}</CardTitle>
+                <CardDescription>{linjeData.year}</CardDescription>
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
       </div>
+      )}
 
-      <div className="grid grid-cols-2 gap-4">
-        {Object.values(data.filter((filterData) => filterData.year == 4)).map((linjeData) => (
-          <Card key={linjeData.id} id={"year_"+linjeData.id.toString()} className="bg-[] rounded-2xl cursor-pointer" onClick={() => handleCardClick(linjeData.id)}>
-            <CardHeader>  
-              <CardTitle>{linjeData.name}</CardTitle>
-              <CardDescription>{linjeData.year}</CardDescription>
-            </CardHeader>
-          </Card>
-        ))}
-      </div>
+
+      {data.filter((filterData) => filterData.year == 4).length >= 1 && (
+        <div>
+          <h1>VG4</h1>
+          <div className="grid grid-cols-2 gap-4">
+            {Object.values(data.filter((filterData) => filterData.year == 4)).map((linjeData) => (
+              <Card key={linjeData.id} id={"year_"+linjeData.id.toString()} className="bg-[] rounded-2xl cursor-pointer" onClick={() => handleCardClick(linjeData.id)}>
+                <CardHeader>  
+                  <CardTitle>{linjeData.name}</CardTitle>
+                  <CardDescription>{linjeData.year}</CardDescription>
+                </CardHeader>
+              </Card>
+            ))}
+          </div>  
+        </div>
+      )}
     </div>
   )
 
