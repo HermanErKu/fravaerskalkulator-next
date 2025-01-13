3491ea3bc1f0f7070d708c3155e36fef8ef4f766
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2025-01-13 20:56:07 +0100

    made calculator kinda working

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table"
 
@@ -12,12 +12,41 @@ interface YearData {
   [key: number]: { name: string; timer?: number };
 }
 
+interface FagData {
+  [key: number]: { name: string; timer?: number };
+}
+
 interface CalculatorProps {
   data: YearData[];
 }
 
 const Calculator: React.FC<CalculatorProps> = ({ data }) => {
-  const [tableView, setTableView] = useState(false);
+  const [tableView, setTableView] = useState(true);
+
+  //console.log(data[0][0]);
+
+  let fagData = []
+  for(let fagId = 0; fagId < data[0].fag; fagId++){
+    fagData.push([data[0][fagId].name, data[0][fagId].timer])
+  }
+
+  const handleChangeTableView = (id:number, value:number) => {
+    let fravær = Math.round((((value*0.75)/(parseInt(fagData[id][1]?.toString() || "1")))*100)*100)/100
+    if (isNaN(fravær)) {
+      fravær = 0;
+    }
+
+    console.log(document.getElementsByClassName(id.toString()))
+    const classElement = document.getElementsByClassName(id.toString())[0];
+    const wholePercentText = classElement ? classElement.querySelector('#whole') : null;
+    if (wholePercentText) {
+      wholePercentText.innerHTML = fravær.toString()+"%";
+    }
+    const halfPercentText = classElement ? classElement.querySelector('#half') : null;
+    if (halfPercentText) {
+      halfPercentText.innerHTML = (fravær*2).toString()+"%";
+    }
+  }
 
   return (
     <div className="">
@@ -27,7 +56,27 @@ const Calculator: React.FC<CalculatorProps> = ({ data }) => {
       {tableView 
       ? 
       <div>
-        <p>Table view not ready yet:</p>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Fag</TableHead>
+              <TableHead>Timer Fri</TableHead>
+              <TableHead>Halvår</TableHead>
+              <TableHead>Total</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+  
+          {fagData.map((fag, id) => (
+            <TableRow key={id} className={id.toString()}>
+              <TableCell id="name">{fag[0]}</TableCell>
+              <TableCell id="input"><input className="w-10" type="number" onChange={(input) => handleChangeTableView(id, parseInt(input.target.value))} /></TableCell>
+              <TableCell id="half">0%</TableCell>
+              <TableCell id="whole">0%</TableCell>
+            </TableRow>
+          ))}
+          </TableBody>
+        </Table>
       </div>
       :
       <div>
@@ -37,13 +86,13 @@ const Calculator: React.FC<CalculatorProps> = ({ data }) => {
 
 
 
-      {data.map((item) => (
+      {/*data.map((item) => (
         <div key={item.id}>
           <h2>{item.name}</h2>
           <p>Year: {item.year}</p>
           <p>Fag: {item.fag}</p>
         </div>
-      ))}
+      ))*/}
     </div>
   );
 };
