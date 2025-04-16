13f142a5cae050c61586e936e4f12df0142c4547
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2025-04-16 14:47:57 +0200

    pwa files

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/vercel.svg" />
       </>
     )
   }
