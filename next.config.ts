8fb920f0b9109c6832aa9176b3bfda147c7fc085
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2025-04-16 14:52:33 +0200

    typo

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,5 @@ const nextConfig: NextConfig = {
 // Wrapp configen med withPWA
 export default withPWA({
   dest: "public",
-  //disable: process.env.NODE_ENV === "development"
-  disable: false
+  disable: process.env.NODE_ENV === "development"
 })(nextConfig)
